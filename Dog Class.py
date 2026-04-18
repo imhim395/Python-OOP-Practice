db69a38075acf74cec0b827a07d7695597b2f16b
imhim395 <yuvaangulati7689@gmail.com>
2026-04-18 10:16:51 -0400

    init commit

--- Dog Class.py
+++ Dog Class.py
@@ -8,8 +8,17 @@ class Dog:
     def bday(self):
         print(f"today, it is {self.name}'s birthday!")#f-string to make the shi cleaner
 
+    def bark(self):
+        if self.is_friendly is False:
+            print(self.name,"is barking at you, arf arf")#bruh
+        else:
+            print(self.name, "is friendly and wants to play with you")
+
+
 dog_one = Dog("pierre", "golden retriever", "5", True)
-dog_two = Dog("carti", "black lab", "7", False)
+dog_two = Dog("sparky", "black lab", "7", False)
 
 dog_one.bday()
 dog_two.bday()
+dog_one.bark()
+dog_two.bark()
